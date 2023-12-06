2146b093edbe168a6af6935d9084416bf453ef8e
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-12-06 18:09:00 +0200

    removed space to test workflow

--- backend/src/main/java/com/simplesolutions/medicinesmanager/MedicinesManagerApplication.java
+++ backend/src/main/java/com/simplesolutions/medicinesmanager/MedicinesManagerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedicinesManagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MedicinesManagerApplication.class, args);
     }
