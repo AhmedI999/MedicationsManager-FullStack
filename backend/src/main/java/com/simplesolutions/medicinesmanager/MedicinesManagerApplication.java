3a86861074e0b491eb9e8ab07933e7e02d06b405
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-12-06 18:21:02 +0200

    testing workflow

--- backend/src/main/java/com/simplesolutions/medicinesmanager/MedicinesManagerApplication.java
+++ backend/src/main/java/com/simplesolutions/medicinesmanager/MedicinesManagerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicinesManagerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(MedicinesManagerApplication.class, args);
     }
+
 
 }
