143bb9098a317cc474a1933418ffdce8ca9e358d
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-12-08 18:34:47 +0200

    fix validation

--- backend/src/main/resources/db/migration/V5__FIx_Patient_Id_Schema_Sequence.sql
+++ backend/src/main/resources/db/migration/V5__FIx_Patient_Id_Schema_Sequence.sql
@@ -4,7 +4,10 @@ ALTER TABLE medicine ALTER COLUMN id DROP DEFAULT;
 -- Drop the default sequence created for medicine id
 DROP SEQUENCE IF EXISTS medicine_id_seq;
 
--- Create a new sequence starting from 1
+-- Alter type of id to INT. it will suffice
+ALTER TABLE medicine ALTER COLUMN id TYPE INTEGER;
+
+-- Create a new sequence starting from 1 with BIGINT type
 CREATE SEQUENCE medicine_id_seq START 1;
 
 -- Set the default value of the id column in medicine to use the new sequence
@@ -12,6 +15,3 @@ ALTER TABLE medicine ALTER COLUMN id SET DEFAULT nextval('medicine_id_seq');
 
 -- Set the current value of the sequence to the maximum existing id in the medicine table
 SELECT setval('medicine_id_seq', COALESCE((SELECT MAX(id) FROM medicine), 1));
-
--- Add the default constraint back on the id column in the medicine table
-ALTER TABLE medicine ALTER COLUMN id SET DEFAULT nextval('medicine_id_seq');
