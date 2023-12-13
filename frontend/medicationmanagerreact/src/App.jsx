14dbe42a3480de6d4e596986fdca27ba8439a064
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-12-13 12:24:34 +0200

    initial project setup

---
+++ frontend/medicationmanagerreact/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@chakra-ui/react'
+function App() {
+  return (
+      <div>
+          <Button colourScheme='blue'>Click me</Button>
+      </div>
+  );
+}
+
+export default App
