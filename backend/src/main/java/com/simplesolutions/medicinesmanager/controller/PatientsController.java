400da2e62c458d6952a0ffde3e3fd3088dda2cfa
AhmedI999 <ahmedibrahim5182@gmail.com>
2024-01-01 20:29:11 +0200

    test cd

--- backend/src/main/java/com/simplesolutions/medicinesmanager/controller/PatientsController.java
+++ backend/src/main/java/com/simplesolutions/medicinesmanager/controller/PatientsController.java
@@ -8,7 +8,6 @@ import com.simplesolutions.medicinesmanager.service.patient.PatientService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
