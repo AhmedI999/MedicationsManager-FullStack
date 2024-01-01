36379828530f770f2546bcb5e7b3478957f327ca
AhmedI999 <ahmedibrahim5182@gmail.com>
2024-01-01 20:46:53 +0200

    test cd

--- backend/src/main/java/com/simplesolutions/medicinesmanager/controller/PatientsController.java
+++ backend/src/main/java/com/simplesolutions/medicinesmanager/controller/PatientsController.java
@@ -17,7 +17,6 @@ import java.util.List;
 @RequestMapping("api/v1/patients")
 @RequiredArgsConstructor
 public class PatientsController {
-
     private final PatientService patientService;
     private final JWTUtil jwtUtil;
 
